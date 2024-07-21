4994e0bcbff47183100bfce86836ee764458e95d
Asguho <78410939+Asguho@users.noreply.github.com>
2024-07-21 20:34:49 +0200

    added fs

---
+++ SourceMate/src/routes/source/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import { readTextFile, BaseDirectory } from '@tauri-apps/plugin-fs';
+    
+    export let data: PageData;
+
+    readTextFile('AppData\\Roaming\\Microsoft\\Bibliography\\Sources.xml', { baseDir: BaseDirectory.Home}).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.error(error);
+    }); 
+</script>
+
