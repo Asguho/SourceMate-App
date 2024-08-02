387f2a8030c936d7f133802c4e09bfd7e7ed1f3e
Asguho <78410939+Asguho@users.noreply.github.com>
2024-08-02 17:13:08 +0700

    Update utils.ts

--- src/lib/scripts/utils.ts
+++ src/lib/scripts/utils.ts
@@ -1,12 +1,12 @@
 // import { parse } from "https://deno.land/x/xml@2.1.1/mod.ts";
-import type { Source } from "./types.ts";
+import type { Source } from "./types.js";
 // import data_dir from "https://deno.land/x/dir@1.5.1/data_dir/mod.ts";
 
 export async function getSource(url: URL): Promise<Source> {
-  return await fetch(
-    "https://auto-references-api.deno.dev/api?url=" +
-      encodeURIComponent(url.toString()),
-  ).then((res) => res.json());
+	return await fetch(
+		"https://auto-references-api.deno.dev/api?url=" +
+			encodeURIComponent(url.toString()),
+	).then((res) => res.json());
 }
 
 // export async function getSiteName(hostname: string) {
@@ -16,59 +16,59 @@ export async function getSource(url: URL): Promise<Source> {
 // }
 
 function getAuthorJson(authors: string[], corporate: boolean = false) {
-  if (corporate) {
-    return ({
-      "b:Corporate": authors[0].trim() || "",
-    });
-  } else {
-    const authorsJson = [];
-    for (const author of authors) {
-      const authorNames = (author.trim() || "").split(" ");
-      authorsJson.push({
-        "b:Person": {
-          "b:First": authorNames[0],
-          "b:Middle": authorNames.slice(1, authorNames.length - 1).join(" "),
-          "b:Last": authorNames[authorNames.length - 1],
-        },
-      });
-    }
-    return ({
-      "b:NameList": authorsJson,
-    });
-  }
+	if (corporate) {
+		return {
+			"b:Corporate": authors[0].trim() || "",
+		};
+	} else {
+		const authorsJson = [];
+		for (const author of authors) {
+			const authorNames = (author.trim() || "").split(" ");
+			authorsJson.push({
+				"b:Person": {
+					"b:First": authorNames[0],
+					"b:Middle": authorNames.slice(1, authorNames.length - 1).join(" "),
+					"b:Last": authorNames[authorNames.length - 1],
+				},
+			});
+		}
+		return {
+			"b:NameList": authorsJson,
+		};
+	}
 }
 
 export function convertToSourceFormat(data: Source) {
-  const guid = crypto.randomUUID().toUpperCase();
-  const date = new Date();
+	const guid = crypto.randomUUID().toUpperCase();
+	const date = new Date();
 
-  console.log("\nData added:");
-  console.log("Author:", getAuthorJson(data.authors, data.corporate));
+	console.log("\nData added:");
+	console.log("Author:", getAuthorJson(data.authors, data.corporate));
 
-  console.log("Title:", data.webPageName);
-  console.log("InternetSiteTitle:", data.webSiteName);
-  if (data.year) console.log("Year:", data.year);
-  if (data.month) console.log("Month:", data.month);
-  if (data.day) console.log("Day:", data.day);
-  console.log("URL:", data.url);
+	console.log("Title:", data.webPageName);
+	console.log("InternetSiteTitle:", data.webSiteName);
+	if (data.year) console.log("Year:", data.year);
+	if (data.month) console.log("Month:", data.month);
+	if (data.day) console.log("Day:", data.day);
+	console.log("URL:", data.url);
 
-  return {
-    "b:Tag": guid,
-    "b:SourceType": "DocumentFromInternetSite",
-    "b:Guid": `{${guid}}`,
-    "b:Author": {
-      "b:Author": getAuthorJson(data.authors, data.corporate),
-    },
-    "b:Title": data.webPageName,
-    "b:InternetSiteTitle": data.webSiteName,
-    "b:Year": data.year,
-    "b:Month": data.month,
-    "b:Day": data.day,
-    "b:URL": data.url,
-    "b:YearAccessed": date.getFullYear(),
-    "b:MonthAccessed": date.getMonth() + 1,
-    "b:DayAccessed": date.getDate(),
-  };
+	return {
+		"b:Tag": guid,
+		"b:SourceType": "DocumentFromInternetSite",
+		"b:Guid": `{${guid}}`,
+		"b:Author": {
+			"b:Author": getAuthorJson(data.authors, data.corporate),
+		},
+		"b:Title": data.webPageName,
+		"b:InternetSiteTitle": data.webSiteName,
+		"b:Year": data.year,
+		"b:Month": data.month,
+		"b:Day": data.day,
+		"b:URL": data.url,
+		"b:YearAccessed": date.getFullYear(),
+		"b:MonthAccessed": date.getMonth() + 1,
+		"b:DayAccessed": date.getDate(),
+	};
 }
 
 // export async function getJson() {
