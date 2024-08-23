1c7b1220c33f5dbffcde8015c145212c55658e60
Asguho <78410939+Asguho@users.noreply.github.com>
2024-08-23 19:00:58 +0200

    update

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use tauri_plugin_updater::UpdaterExt;
+// use tauri_plugin_updater::UpdaterExt;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
-let url = $state("");
+  let url = $state("");
 
-let { data: propData } = $props();
-const { session, supabase } = propData;
+  let { data: propData } = $props();
+  const { session, supabase } = propData;
 </script>
+
 <div class="flex justify-center items-center min-h-screen">
   <div class="">
     <h1 class="font-extrabold text-4xl tracking-tight mb-4">Enter a url you want to add!</h1>
-    <form class="grid grid-cols-3" method="get" action="/source" >
-      <input type="text" autocomplete="url" name="url" class="input input-bordered col-span-2" bind:value={url}>
+    <form class="grid grid-cols-3" method="get" action="/source">
+      <input type="text" autocomplete="url" name="url" class="input input-bordered col-span-2" bind:value={url} />
       <button type="submit" class="btn ml-2">Crawl Source</button>
     </form>
   </div>
 </div>
 
-<a href="https://github.com/Asguho/SourceMate-App/issues/new/choose" class="btn btn-outline absolute bottom-4 right-4">Report a Bug or Give Feedback!</a>
+<a href="https://github.com/Asguho/SourceMate-App/issues/new/choose" target="_blank" class="btn btn-outline absolute bottom-4 right-4"
+  >Report a Bug or Give Feedback!</a
+>
