adfce20fb3c2ee31442f9a0525aca176a06174a6
Asguho <78410939+Asguho@users.noreply.github.com>
2024-08-23 18:55:48 +0200

    update

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,47 +3,48 @@
 use tauri_plugin_updater::UpdaterExt;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        // .setup(|app| {
+        //     let handle = app.handle().clone();
+        //     tauri::async_runtime::spawn(async move {
+        //         update(handle).await;
+        //     });
+        //     Ok(())
+        // })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
+// async fn update(app: tauri::AppHandle) -> tauri::Result<()> {
+//     if let Some(update) = app
+//         .updater()
+//         .map_err(|e| tauri::Error::from(e))?
+//         .check()
+//         .await?
+//     {
+//         let mut downloaded = 0;
 
-fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      let handle = app.handle().clone();
-      tauri::async_runtime::spawn(async move {
-        update(handle).await;
-      });
-      Ok(())
-    })
-}
-async fn update(app: tauri::AppHandle) -> tauri::Result<()> {
-  if let Some(update) = app.updater()?.check().await? {
-    let mut downloaded = 0;
+//         // alternatively we could also call update.download() and update.install() separately
+//         update
+//             .download_and_install(
+//                 |chunk_length, content_length| {
+//                     downloaded += chunk_length;
+//                     println!("downloaded {downloaded} from {content_length:?}");
+//                 },
+//                 || {
+//                     println!("download finished");
+//                 },
+//             )
+//             .await?;
 
-    // alternatively we could also call update.download() and update.install() separately
-    update.download_and_install(|chunk_length, content_length| {
-      downloaded += chunk_length;
-      println!("downloaded {downloaded} from {content_length:?}");
-    }, || {
-      println!("download finished");
-    }).await?;
+//         println!("update installed");
+//         app.restart();
+//     }
 
-    println!("update installed");
-    app.restart();
-  }
-
-  Ok(())
-}
+//     Ok(())
+// }
